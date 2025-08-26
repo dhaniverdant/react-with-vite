3b4c70abbe05252fdf544335aa438996f93567f6
dhaniverdant <dhaniverdant23@gmail.com>
2025-08-27 05:54:53 +0800

    remake all: create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-
-const Card = ({ title }) => {
-  const [hasLiked, setHasLiked] = useState(false);
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  }, [hasLiked, title]);
-
-  return (
-    <div className="card" onClick={() => setCount(count + 1)}>
-      <h2>{title} <br/>{count}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? '💖' : '🖤'}
-      </button>
-    </div>
-  )
-}
+import React from 'react'
 
 const App = () => {
   return (
-    <div className="card-container">
-      <Card title="One Piece" />
-      <Card title="Dragon Ball" />
-      <Card title="Naruto" />
-    </div>
+    <main>
+      <div className='pattern' />
+
+      <div className="wrapper">
+        <header>
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without The Hassle</h1>
+        </header>
+      </div>
+    </main>
   )
 }
 
