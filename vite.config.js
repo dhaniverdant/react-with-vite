acaafd90bd08b5ecc038907f3c452de37dad2bdc
dhaniverdant <dhaniverdant23@gmail.com>
2025-09-09 20:09:28 +0800

    fix config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
-  base: import.meta.env.VITE_BASE_PATH || "/react-with-vite",
-})
+  base: mode === 'production' ? '/react-with-vite/' : '/',
+}))
